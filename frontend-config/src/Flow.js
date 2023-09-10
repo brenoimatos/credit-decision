a95b39151694b1631e635f7f8475cd0867d675fd
brenoimatos <70553244+brenoimatos@users.noreply.github.com>
2023-09-10 09:23:42 -0300

    creating drag and drop nodes

--- frontend-config/src/Flow.js
+++ frontend-config/src/Flow.js
@@ -1,74 +1,95 @@
-import { useCallback, useState } from 'react';
-import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
+import React, { useCallback, useState, useRef } from 'react';
+import ReactFlow, { ReactFlowProvider, addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
 import 'reactflow/dist/style.css';
-import DiamondNode from "./components/Shapes/DiamondNode";
+import DiamondNode from "./components/CustomNodes/DiamondNode";
+import { initialEdges, initialNodes } from './utils/constants';
+import Sidebar from './components/Sidebar';
+import StartNode from './components/CustomNodes/StartNode';
+import EndNode from './components/CustomNodes/EndNode';
 
-const initialNodes = [
-  {
-    id: "1",
-    type: "input",
-    data: { label: "Start" },
-    position: { x: 250, y: 0 }
-  },
-  {
-    id: "2",
-    type: "diamond",
-    data: { label: "Age" },
-    position: { x: 300, y: 150 }
-  },
-  {
-    id: "3",
-    type: "diamond",
-    data: { label: "Income" },
-    position: { x: 300, y: 300 }
-  },
-  {
-    id: "4",
-    type: "output",
-    data: { label: "End" },
-    position: { x: 250, y: 450 }
-  },
-  
-];
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 
 const nodeTypes = {
     diamond: DiamondNode,
+    start: StartNode,
+    end: EndNode
   };
 
-  const initialEdges = [
-    { id: "e1-2", source: "1", sourceHandle: "a", target: "2", label: "Test label" },
-    { id: "e2c-3", source: "2", sourceHandle: "c", target: "3", label: "Test label" }
-  ];
-  
 
 const BasicFlow = () => {
+    const reactFlowWrapper = useRef(null);
     const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
-
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes]
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
-  const onConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges]
-  );
-
-  return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      nodeTypes={nodeTypes}
-      fitView
-    />
-  );
-}
+    const [edges, setEdges] = useState(initialEdges);
+  
+    const onNodesChange = useCallback(
+      (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+      []
+    );
+    const onEdgesChange = useCallback(
+      (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+      []
+    );
+    const onConnect = useCallback((connection) => {
+        const newConnection = { ...connection };
+    
+        if (connection.sourceHandle === 'true') {
+            newConnection.label = 'True';
+        } else if (connection.sourceHandle === 'false') {
+            newConnection.label = 'False';
+        }
+        setEdges((eds) => addEdge(newConnection, eds));
+    }, [setEdges]);
+  
+    const onDragOver = useCallback((event) => {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'move';
+    }, []);
+  
+    const onDrop = useCallback(
+      (event) => {
+        event.preventDefault();
+  
+        const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+        const type = event.dataTransfer.getData('application/reactflow');
+        const position = {
+          x: event.clientX - reactFlowBounds.left,
+          y: event.clientY - reactFlowBounds.top,
+        };
+  
+        const newNode = {
+          id: getId(),
+          type,
+          position,
+          data: { label: `${type} node` },
+        };
+  
+        setNodes((nds) => nds.concat(newNode));
+      },
+      [reactFlowWrapper, setNodes]
+    );
+  
+    return (
+      <div className="dndflow">
+        <ReactFlowProvider>
+          <Sidebar />
+          <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+            <ReactFlow
+              nodes={nodes}
+              edges={edges}
+              onNodesChange={onNodesChange}
+              onEdgesChange={onEdgesChange}
+              onConnect={onConnect}
+              nodeTypes={nodeTypes}
+              onDragOver={onDragOver}
+              onDrop={onDrop}
+              fitView
+            />
+          </div>
+        </ReactFlowProvider>
+      </div>
+    );
+};
+  
   
 export default BasicFlow;
---
+++ frontend-config/src/components/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Sidebar = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <aside>
+      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'start')} draggable>
+        Start Node
+      </div>
+      <div className="dndnode diamond" onDragStart={(event) => onDragStart(event, 'diamond')} draggable>
+        Decision Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'end')} draggable>
+        End Node
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
